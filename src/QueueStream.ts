0f63099cdfc82ac2eb3a8a9856437b7dce7f7cb1
sokolw <sokolw21@gmail.com>
2022-06-24 19:51:47 +0300

    feat: implement queue

--- src/QueueStream.ts
+++ src/QueueStream.ts
@@ -1,5 +1,20 @@
 import stream from "stream";
 
 export class QueueStream extends stream.Duplex {
-  
+  data: any = [];
+
+  _write(chunk: any, enc: BufferEncoding, next: (error?: Error | null) => void): void {
+    this.data.push(chunk);
+    console.log(this.data.length, ' queue');
+    next();
+  }
+
+  _read(size: number): void {
+    this.push(this.data.shift());
+    // if (this.data.length !== 0){
+    //   this.push(this.data.shift());
+    // } else {
+    //   this.push(null);
+    // }
+  }
 }
