c99c000861c6b0d70a9bafb4d5511ce4511ae052
sokolw <sokolw21@gmail.com>
2022-06-24 19:52:30 +0300

    feat: add enum commands

--- src/RemoteCommands.ts
+++ src/RemoteCommands.ts
@@ -1,3 +1,11 @@
 export enum RemoteCommands {
-  
+  MouseUp = 'mouse_up',
+  MouseDown = 'mouse_down',
+  MouseLeft = 'mouse_left',
+  MouseRight = 'mouse_right',
+  MousePosition = 'mouse_position',
+  DrawCircle = 'draw_circle',
+  DrawRectangle = 'draw_rectangle',
+  DrawSquare = 'draw_square',
+  PrintScrn = 'prnt_scrn',
 }
