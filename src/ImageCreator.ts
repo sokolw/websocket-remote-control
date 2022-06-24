c683de48d6417fde27c265564aa47e0be53790de
sokolw <sokolw21@gmail.com>
2022-06-24 19:53:05 +0300

    feat: implement image creator

---
+++ src/ImageCreator.ts
@@ -0,0 +1,28 @@
+import Jimp from 'jimp';
+import robot from 'robotjs';
+import { Point } from './Point.js';
+
+export class ImageCreator {
+  public async createScreenshot(): Promise<string> {
+    const mousePoint: Point = robot.getMousePos();
+    const size: number = 200;
+    const image: robot.Bitmap = robot.screen.capture(
+      mousePoint.x - size / 2,
+      mousePoint.y - size / 2,
+      size, size
+    );
+
+    for(let i = 0; i < image.image.length; i++) {
+      if (i % 4 === 0) {
+        const temp = image.image[i];
+        image.image[i] = image.image[i + 2];
+        image.image[i + 2] = temp;
+      }
+    }
+
+    const jimp = new Jimp({ data: image.image, width: size, height: size });
+    const base64 = (await jimp.getBase64Async(Jimp.MIME_PNG)).split(',')[1];
+
+    return base64;
+  }
+}
