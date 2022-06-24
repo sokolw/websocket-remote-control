e105b4d65667bec18fd31c66049519aa22ed18cb
sokolw <sokolw21@gmail.com>
2022-06-24 20:51:06 +0300

    feat: add .env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { ServerFactory } from './ServerFactory.js';
+import { config } from "dotenv";
+
+const setEnv = () : void => {
+  config();
+  if (!process.env.PORT) {
+    process.exit(1);
+  }
+}
+setEnv();
 
 // run app
-const app : ServerFactory = new ServerFactory(5000);
+const app : ServerFactory = new ServerFactory(parseInt(process.env.PORT as string, 10));
 app.start();
