d3009ab5f13b43a76ff1071d38e6471b7b079530
sokolw <sokolw21@gmail.com>
2023-01-21 22:56:21 +0300

    feat: add method changePosByPlatform

--- src/MouseController.ts
+++ src/MouseController.ts
@@ -7,22 +7,26 @@ import { Point } from './Point';
 export class MouseController {
   public async mouseUp(point: Point): Promise<void> {
     const mousePosition = await mouse.getPosition();
-    await mouse.setPosition(new Point(mousePosition.x + 1, mousePosition.y - point.y + 1));
+    const resultPosition = new Point(mousePosition.x, mousePosition.y - point.y);
+    await mouse.setPosition(resultPosition.changePosByPlatform());
   }
 
   public async mouseDown(point: Point): Promise<void> {
     const mousePosition = await mouse.getPosition();
-    await mouse.setPosition(new Point(mousePosition.x + 1, mousePosition.y + point.y + 1));
+    const resultPosition = new Point(mousePosition.x, mousePosition.y + point.y);
+    await mouse.setPosition(resultPosition.changePosByPlatform());
   }
 
   public async mouseLeft(point: Point): Promise<void> {
     const mousePosition = await mouse.getPosition();
-    await mouse.setPosition(new Point(mousePosition.x - point.x + 1, mousePosition.y + 1));
+    const resultPosition = new Point(mousePosition.x - point.x, mousePosition.y);
+    await mouse.setPosition(resultPosition.changePosByPlatform());
   }
 
   public async mouseRight(point: Point): Promise<void> {
     const mousePosition = await mouse.getPosition();
-    await mouse.setPosition(new Point(mousePosition.x + point.x + 1, mousePosition.y + 1));
+    const resultPosition = new Point(mousePosition.x + point.x, mousePosition.y);
+    await mouse.setPosition(resultPosition.changePosByPlatform());
   }
 
   public async mousePosition(): Promise<Point> {
@@ -35,7 +39,7 @@ export class MouseController {
     for (let i = 0; i <= Math.PI * 2; i += 0.0025) {
       const x = mousePosition.x + (figure.size / 2) * Math.cos(i);
       const y = mousePosition.y + (figure.size / 2) * Math.sin(i);
-      await mouse.setPosition(new Point(x + 1, y + 1));
+      await mouse.setPosition(new Point(x, y).changePosByPlatform());
       if (i === 0) {
         await mouse.pressButton(Button.LEFT);
       }
@@ -47,17 +51,17 @@ export class MouseController {
     const mousePosition = await mouse.getPosition();
     const speed = 0.05;
     for (let i = 0; i <= figure.width; i += speed) {
-      await mouse.setPosition(new Point(mousePosition.x + i + 1, mousePosition.y + 1));
+      await mouse.setPosition(new Point(mousePosition.x + i, mousePosition.y).changePosByPlatform());
       if (i === 0) {
         await mouse.pressButton(Button.LEFT);
       }
     }
     for (let i = 0; i <= figure.height; i += speed)
-      await mouse.setPosition(new Point(mousePosition.x + figure.width + 1, mousePosition.y + i + 1));
+      await mouse.setPosition(new Point(mousePosition.x + figure.width, mousePosition.y + i).changePosByPlatform());
     for (let i = mousePosition.x + figure.width; i >= mousePosition.x; i -= speed)
-      await mouse.setPosition(new Point(i + 1, mousePosition.y + figure.height + 1));
+      await mouse.setPosition(new Point(i, mousePosition.y + figure.height).changePosByPlatform());
     for (let i = mousePosition.y + figure.height; i >= mousePosition.y; i -= speed)
-      await mouse.setPosition(new Point(mousePosition.x + 1, i + 1));
+      await mouse.setPosition(new Point(mousePosition.x, i).changePosByPlatform());
 
     await mouse.releaseButton(Button.LEFT);
   }
@@ -66,17 +70,17 @@ export class MouseController {
     const mousePosition = await mouse.getPosition();
     const speed = 0.05;
     for (let i = 0; i <= figure.size; i += speed) {
-      await mouse.setPosition(new Point(mousePosition.x + i + 1, mousePosition.y + 1));
+      await mouse.setPosition(new Point(mousePosition.x + i, mousePosition.y).changePosByPlatform());
       if (i === 0) {
         await mouse.pressButton(Button.LEFT);
       }
     }
     for (let i = 0; i <= figure.size; i += speed)
-      await mouse.setPosition(new Point(mousePosition.x + figure.size + 1, mousePosition.y + i + 1));
+      await mouse.setPosition(new Point(mousePosition.x + figure.size, mousePosition.y + i).changePosByPlatform());
     for (let i = mousePosition.x + figure.size; i >= mousePosition.x; i -= speed)
-      await mouse.setPosition(new Point(i + 1, mousePosition.y + figure.size + 1));
+      await mouse.setPosition(new Point(i, mousePosition.y + figure.size).changePosByPlatform());
     for (let i = mousePosition.y + figure.size; i >= mousePosition.y; i -= speed)
-      await mouse.setPosition(new Point(mousePosition.x + 1, i + 1));
+      await mouse.setPosition(new Point(mousePosition.x, i).changePosByPlatform());
 
     await mouse.releaseButton(Button.LEFT);
   }
--- src/Point.ts
+++ src/Point.ts
@@ -1,5 +1,20 @@
 export class Point {
-  constructor(public readonly x: number, public readonly y: number) {}
+  constructor(public x: number, public y: number) {}
+
+  changePosByPlatform() {
+    switch (process.platform) {
+      case 'win32':
+        this.x += 1;
+        this.y += 1;
+        return this;
+      case 'linux':
+        return this;
+      case 'darwin':
+        return this;
+      default:
+        return this;
+    }
+  }
 
   toString() {
     return `(${this.x}, ${this.y})`;
